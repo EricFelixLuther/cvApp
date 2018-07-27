bffb9e9844ccb2f3580a849deefb1def7927535e
Krzysztof Maciejczuk <ericfelixluther@gmail.com>
2018-07-27 12:00:32 +0200

    Working on view. added admin models.

--- cv_App/cvAppMain/admin.py
+++ cv_App/cvAppMain/admin.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from cvApp.cvAppMain.models import Summary_Text, IT_Tools, , IT_Tools_List,\
+from cv_App.cvAppMain.models import Summary_Text, IT_Tools, , IT_Tools_List,\
     IT_Skills, Hobbies, Languages, Other_Job_Experience, Job_Experience,\
     Job_Details, Education, Courses, Company
+
 
 admin.register((Summary_Text, IT_Tools, IT_Tools_List, IT_Skills, Hobbies, Languages, Other_Job_Experience,
                 Job_Experience, Job_Details, Education, Courses, Company))
--- cv_App/cvAppMain/views.py
+++ cv_App/cvAppMain/views.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class CV_pl(View):
+    pass
+
+
+class CV_eng(View):
+    pass
+
+
+class CV_export(View):
+    pass
