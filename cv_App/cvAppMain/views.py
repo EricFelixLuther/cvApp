3de984245756b3bb54f2fd66d77ff8ff9801989c
Krzysztof Maciejczuk <maciejczuk.krzysztof@gmail.com>
2019-09-01 20:47:49 +0200

    Experimental PDF feature

--- cv_App/cvAppMain/views.py
+++ cv_App/cvAppMain/views.py
@@ -1,7 +1,12 @@
+from io import BytesIO
+
 import markdown2
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.views import View
+from xhtml2pdf import pisa
 
 from cvAppMain.forms import CompanySelectForm
 from cvAppMain.models import BaseData
@@ -18,6 +23,15 @@ class CV_Viewer(View):
         )
 
     def post(self, request, *args, **kwargs):
+        btn = request.POST.get("submit", False)
+        if btn == "get_cv":
+            return self._get_view(request)
+        elif btn == "print_cv":
+            return self._get_pdf(request)
+        else:
+            return HttpResponse("Bad request!", status=400)
+
+    def _get_view(self, request):
         form = self.form(data=request.POST)
         if form.is_valid():
             context = {
@@ -41,6 +55,28 @@ class CV_Viewer(View):
                 context={"form": form}
             )
 
-
-class CV_export(View):
-    pass
+    def _get_pdf(self, request):
+        form = self.form(data=request.POST)
+        if form.is_valid():
+            context = {
+                "company": form.company,
+                "base_data": BaseData
+            }
+            for each in form.company.texts.filter(language=form.cleaned_data["language"]):
+                if each.markdown:
+                    context[each.text_type.codename] = mark_safe(markdown2.markdown(each.text, extras=["tables"]))
+                else:
+                    context[each.text_type.codename] = mark_safe(each.text)
+            html = render_to_string(form.company.document.name, context)
+            result = BytesIO()
+            pdf = pisa.pisaDocument(html, dest=result)
+            if not pdf.err:
+                return HttpResponse(result.getvalue(), content_type='application/pdf')
+            else:
+                return HttpResponse('Error while generating PDF! Sorry!')
+        else:
+            return render(
+                request,
+                template_name=self.template_name,
+                context={"form": form}
+            )
