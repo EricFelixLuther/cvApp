d3b40fa284f8336c8cbd13c17ec1bab1328d4be6
Krzysztof Maciejczuk <maciejczuk.krzysztof@gmail.com>
2020-02-12 14:52:24 +0100

    Added flatpages functionality

--- cv_App/cvApp/settings.py
+++ cv_App/cvApp/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'cvApp.urls'
--- cv_App/cvApp/urls.py
+++ cv_App/cvApp/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 
 from cvAppMain.views import CV_Viewer
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', CV_Viewer.as_view()),
-]
+    path('admin/', admin.site.urls),
+    path('get_cv/', CV_Viewer.as_view()),
+    ]
